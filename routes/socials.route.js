a04d361f3af6f265ec6b88fafdf78ba2fd31f535
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-01-25 19:52:17 +0800

    Add starter code for socials route

---
+++ routes/socials.route.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+const socialsRouter = Router();
+
+//getAllSocials
+socialsRouter.get('/', );
+
+//addNewSocial
+socialsRouter.post('/');
+
+//editSocial
+socialsRouter.put('/:socialsName', );
+
+//deleteSocials
+
+socialsRouter.put('/:socialsName', );
