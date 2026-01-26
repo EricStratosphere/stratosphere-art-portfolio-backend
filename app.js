857e38e0145ffded9cc177bd963fa91dc3b5364c
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-01-26 21:56:37 +0800

    Successfully connect Mongoose ORM to MongoDB atlas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import serviceRouter from './routes/service.route.js';
 import experienceRouter from './routes/education-experience.route.js';
 import mainPageRouter from './routes/mainpage.route.js';
 import socialsRouter from './routes/socials.route.js';
+import connectDB from './database/mongodb.js';
 import { PORT, MONGODB_URI } from './config/env.js';
 
 const app = express();
@@ -24,5 +25,6 @@ app.get('/', (req, res)=> {
 
 app.listen(PORT, async()=>{
     console.log(`Stratosphere\'s art backend running on http://localhost:${PORT}`);
+    connectDB();
 });
 export default app;
