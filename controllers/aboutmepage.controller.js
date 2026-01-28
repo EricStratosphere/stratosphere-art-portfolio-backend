a3bab7a019e50c099bae296df70fa148ce4a817c
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2026-01-28 21:09:34 +0800

    Add implementation for aboutmepage controller.

--- controllers/aboutmepage.controller.js
+++ controllers/aboutmepage.controller.js
@@ -1,15 +1,67 @@
-export const editBriefIntroduction = (req, res, next) => {
-    res.status(200).json({
-        success : true,
-        message : "Edited brief introduction!",
-    })
+import AboutMeDetails from "../models/aboutmedetails.model.js";
+export const getAboutMeDetails = async(req, res, next) => {
+    try{
+        const dataArray = await AboutMeDetails.find();
+        if(!dataArray){
+            const error = new Error('No data fetched!');
+            error.statusCode = 400;
+            throw error;
+        }
+        const data = dataArray[0];
+        res.status(200).send(
+            {
+                success : true,
+                data : data,
+            }
+        )
+    }
+    catch(error){
+        next(error);
+    }
+} 
+
+export const editBriefIntroduction = async (req, res, next) => {
+    try{
+        const dataArray = await AboutMeDetails.find();
+        if(!dataArray){
+            const error = new Error('No data fetched!');
+            error.statusCode = 400;
+            throw error;
+        }
+        const data = dataArray[0];
+        data.brief_introduction = req.body.brief_introduction;
+        data.save();
+        res.status(200).send(
+            {
+                success : true,
+                data : data,
+            }
+        )
+    }
+    catch(error){
+        next(error);
+    }
 }
 
-export const editLongIntroduction = (req, res, next) => {
-    res.status(200).json(
-        {
-            success : true,
-            message : "Edited long introduction",
+export const editLongIntroduction = async (req, res, next) => {
+    try{
+        const dataArray = await AboutMeDetails.find();
+        if(!dataArray){
+            const error = new Error('No data fetched!');
+            error.statusCode = 400;
+            throw error;
         }
-    )
+        const data = dataArray[0];
+        data.long_introduction = req.body.long_introduction;
+        data.save();
+        res.status(200).send(
+            {
+                success : true,
+                data : data,
+            }
+        )
+    }
+    catch(error){
+        next(error);
+    }
 }
--- routes/aboutmepage.route.js
+++ routes/aboutmepage.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { editBriefIntroduction, editLongIntroduction } from "../controllers/aboutmepage.controller.js";
+import { editBriefIntroduction, editLongIntroduction, getAboutMeDetails } from "../controllers/aboutmepage.controller.js";
 const aboutMeRouter = Router();
 
+aboutMeRouter.get('/', getAboutMeDetails)
 aboutMeRouter.put('/brief-introduction', editBriefIntroduction);
 
 aboutMeRouter.put('/long-introduction', editLongIntroduction);
